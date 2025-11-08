a7561d547f9dcd31a1d89574631457c023151a3e
juan <juanjuangmo@gmail.com>
2025-11-08 13:54:27 +0100

    Implemented api service

---
+++ domain/services/api_service.ts
@@ -0,0 +1,8 @@
+
+// General API service interface
+export interface ApiService {
+  get: <T>(endpoint: string, params?: Record<string, any>, auth?: boolean) => Promise<T>;
+  post: <T>(endpoint: string, body?: Record<string, any>, auth?: boolean) => Promise<T>;
+  put: <T>(endpoint: string, body?: Record<string, any>, auth?: boolean) => Promise<T>;
+  delete: <T>(endpoint: string, auth?: boolean) => Promise<T>;
+}
