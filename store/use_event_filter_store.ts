a069d72cd695f2fb629a1eb786fcfd8f11cb2ebe
juan <juanjuangmo@gmail.com>
2025-11-10 10:14:10 +0100

    Add useEventFilterStore for global state management of event filter selection

---
+++ store/use_event_filter_store.ts
@@ -0,0 +1,18 @@
+import { FilterTag } from "@/domain/model/enums/filter_tag";
+import { EventRepository } from "@/domain/repository/event_repository";
+import { create } from "zustand";
+
+// Global state management of the filter selection
+interface EventFilterState {
+    actualFilter: FilterTag
+    setFilterState: (newFilter: FilterTag) => void
+}
+
+export const useEventFilterStore = create<EventFilterState>((set, get) => ({
+    actualFilter: FilterTag.Interests, // Default filter
+
+    setFilterState: (newFilter: FilterTag) => {
+        if(newFilter === get().actualFilter) return
+        set({actualFilter: newFilter})
+    }
+}))
